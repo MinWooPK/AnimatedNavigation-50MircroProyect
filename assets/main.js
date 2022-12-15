c7527512bcf41453f7529d96d87acc173d3e80de
MinWooPK <minwoowork91@gmail.com>
2022-12-15 22:05:25 +0100

    final

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+const arrowToogle = document.getElementById('toggle')
+const navBox = document.getElementById('nav')
+
+toggle.addEventListener('click', () =>
+    navBox.classList.toggle('active')
+    // console.log('hello there')
+)
+
+
+
